5078fcc1128a691371778bfdf0c1d815451d914e
Fajar Satria <fajarsat@hotmail.com>
2022-09-29 20:22:48 +0700

    Update index.js
    fix upload

--- src/screens/Plus/index.js
+++ src/screens/Plus/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet ,Text, View, Button, Image} from 'react-native';
 import { Camera } from 'expo-camera';
 import { Video } from 'expo-av';
-
+import axios from 'axios';
 
 
 export default function App() {
@@ -29,18 +29,40 @@ export default function App() {
   const takeVideo = async () => {
     if(camera){
         const data = await camera.recordAsync({
-          maxDuration:10
+          maxDuration:2
         })
         setRecord(data.uri);
         console.log(data.uri);
     }
   }
 
-  const stopVideo = async () => {
-    camera.stopRecording();
+  const uploadVideo = async () => {
+		console.log(record.uri);
+
+		const form = new FormData();
+
+		form.append('videoFeed', {
+			uri: record,
+			type: 'video/mp4',
+			name: 'test'
+		});
+
+		axios({
+			url: 'http://172.104.181.250/uploads',
+			method: 'POST',
+			data: form
+		})
+			.then((response) => {
+				console.log(response);
+				alert(JSON.stringify(response.data));
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+
   }
 
-  const uploadVideo = async () => {
+  const stopVideo = async () => {
     camera.stopRecording();
   }
 
@@ -90,8 +112,8 @@ export default function App() {
             }}>
           </Button>
           <Button title="Take video" onPress={() => takeVideo()} />
-          <Button title="Stop Video" onPress={() => stopVideo()} />
-          <Button title="Upload Video" onPress={() => stopVideo()} />
+          <Button title="Stop Record" onPress={() => stopVideo()} />
+          <Button title="Upload Video" onPress={() => uploadVideo()} />
           
     </View>
   );
